f5014e9d8c9f7948619e0ac31f7fd745ff75bbd3
Akash MP <akashmpnlr@gmail.com>
2025-03-26 05:53:02 +0530

    kainjeyyyy

--- Client/src/Components/Contact/Contact.js
+++ Client/src/Components/Contact/Contact.js
@@ -7,28 +7,28 @@ const Contact = () => {
       <div className="contactUs-left-para">
         <h3>Let's get in touch</h3>
         <i class="fa fa-envelope"></i>
-        <a class="mail-links" href="mailto:kashifkzmi5@gmail.com">
-          kashifkzmi5@gmail.com
+        <a class="mail-links" href="mailto:akashmpnlr@gmail.com">
+          akashmpnlr@gmail.com
         </a>
 
         <i class="fa fa-linkedin"></i>
-        <a class="mail-links" href="https://www.linkedin.com/in/kashiekzmi/">
-          User Name: kashiekzmi
+        <a class="mail-links" href="https://www.linkedin.com/in/akash-mp-62a295270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+          User Name: akash mp
         </a>
 
         <i class="fa fa-github"></i>
-        <a class="mail-links" href="https://github.com/KaShiekzmi">
-          KaShiekzmi
+        <a class="mail-links" href="https://github.com/akashmp2004">
+          akash thenney
         </a>
 
         <i class="fa fa-instagram"></i>
-        <a class="mail-links" href="https://www.instagram.com/kaxhie_x/">
-          @kaxhie_x
+        <a class="mail-links" href="https://www.instagram.com/it.soe_?igsh=OXhpdXh6Yzk1am8=">
+          @ellam_akash
         </a>
 
         <i class="fa fa-phone"></i>
         <a class="mail-links" href="tel:+923019583959">
-          +92 301 9583959
+          +91 1256987598
         </a>
       </div>
       <div className="contactUs-pic">
--- Client/src/Components/Footer/Footer.js
+++ Client/src/Components/Footer/Footer.js
@@ -14,14 +14,14 @@ const Footer = (props) => {
       <div className="below-footer">
         <p>
           You can reach me at{" "}
-          <a className="mail-links" href="mailto:kashifkzmi5@gmail.com">
-            kashifkzmi5@gmail.com
+          <a className="mail-links" href="mailto:akashmpnlr@gmail.com">
+            akashmpnlr@gmail.com
           </a>
         </p>
         <p>
           <a
             className="contact-links"
-            href="https://www.linkedin.com/in/kashiekzmi/"
+            href="https://www.linkedin.com/in/akash-mp-62a295270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -30,7 +30,7 @@ const Footer = (props) => {
           &nbsp;&nbsp;|&nbsp;&nbsp;
           <a
             className="contact-links"
-            href="https://github.com/KaShiekzmi"
+            href="https://github.com/akashmp2004"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -39,7 +39,7 @@ const Footer = (props) => {
           &nbsp;&nbsp;|&nbsp;&nbsp;
           <a
             className="contact-links"
-            href="https://www.instagram.com/kaxhie_x/"
+            href="https://www.instagram.com/it.soe_?igsh=OXhpdXh6Yzk1am8="
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -48,14 +48,14 @@ const Footer = (props) => {
           &nbsp;&nbsp;|&nbsp;&nbsp;
           <a
             className="contact-links"
-            href="https://api.whatsapp.com/qr/GXRHM7PEPNOKA1?autoload=1&app_absent=0"
+            href="https://www.instagram.com/ajaz_c_k_?igsh=OTFoOGRocWZrc29x"
             target="_blank"
             rel="noopener noreferrer"
           >
             <i className="fa fa-whatsapp"></i> WhatsApp
           </a>
         </p>
-        <p>&copy; 2023 Kashif Abbas Kazmi</p>
+        <p>&copy; Project Done</p>
       </div>
     </footer>
   );
